4256a793f21ee95a2122ede663ad2274f6e289a6
DevCiel <developer@devciel.com>
2025-09-15 02:49:49 +0530

    feat: update doctype for createing and link

---
+++ clinicapp/clinicapp/doctype/sr_patient_disable_reason/sr_patient_disable_reason.py
@@ -0,0 +1,6 @@
+# apps/clinicapp/clinicapp/doctype/patient_disable_reason/patient_disable_reason.py
+import frappe
+from frappe.model.document import Document
+
+class PatientDisableReason(Document):
+    pass
